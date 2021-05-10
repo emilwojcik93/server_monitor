8a02485b4557a725e60078675af6733a930efa35
Emil Wojcik <emilwojcik93@gmail.com>
2021-05-11 00:22:06 +0200

    Add files to new repo

---
+++ README.md
@@ -0,0 +1,49 @@
+##Introduction
+
+This script is used to control server power according to critical services status.
+Services which are checked by script:
+- qBittorrent Nox
+- SMB connections
+- SSH connections
+
+
+## Requipments
+This script is written in python3, to work properly it need interpreter and dependencies
+```
+sudo apt-get update && sudo apt-get install \
+python3 python3-pip python3-systemd \
+python3-software-properties software-properties-common \
+ca-certificates tar openssh-server bash curl wget \
+libsystemd-dev gcc make build-essential libc6 libstdc++6 libssl-dev libffi-dev python3-dev
+
+sudo pip3 install systemd
+```
+### Installation
+This script is written to execute as systemd-service. To configure it properly please follow above steps:
+1. Download script to location `/opt/server_check`
+
+2. Create systemd-service by executing following commnad from **root user**:
+```
+cat <<EOF > /lib/systemd/system/server_check.service
+[Unit]
+Description=Server Check Service
+Wants=network.target
+After=network.target
+
+[Service]
+Type=idle
+ExecStart=/usr/bin/python3 /opt/server_check.py
+Restart=always
+TimeoutStartSec=10
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+```
+3. Enable and start systemd-service by:
+```
+sudo systemctl daemon-reload
+sudo systemctl enable server_check.service
+sudo systemctl start server_check.service
+```
---
+++ server_check.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# service name (also used for logging purpose in journalctl)
+import server_check
+import os
+import json
+import re
+import requests
+import subprocess
+import time
+import logging
+from systemd.journal import JournaldLogHandler
+
+# get an instance of the logger object this module will use
+logger = logging.getLogger(__name__)
+
+# instantiate the JournaldLogHandler to hook into systemd
+journald_handler = JournaldLogHandler()
+
+# set a formatter to include the level name
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+
+# add the journald handler to the current logger
+logger.addHandler(journald_handler)
+
+# optionally set the logging level
+logger.setLevel(logging.DEBUG)
+
+while True:
+    def empty_downloads():
+        username = "username"
+        password = "password"
+
+        session_req = requests.post('http://192.168.1.4:8080/api/v2/auth/login', data={"username": username, "password": password})
+        session_cookie = re.findall(r"SID=(.*); Http", session_req.headers['set-cookie'])[0]
+
+        status_req = requests.get('http://192.168.1.4:8080/api/v2/transfer/info', cookies = {"SID": session_cookie})
+        current_speed = json.loads(status_req.text)['dl_info_speed']
+
+        if current_speed == 0:
+#            logger.info("Server dosn't download anything")
+            ping_result = ['ping', '-c', '1', '8.8.8.8']
+
+            return subprocess.call(ping_result, stdout=subprocess.DEVNULL) == 0
+        else:
+            return False
+
+    def samba_has_connected_clients():
+        status_smb = ['smbstatus', '--locks']
+        if len(subprocess.check_output(status_smb).decode('utf-8').split('\n')) > 3:
+            return True 
+        else:
+#            logger.info("Samba has no clients")
+            return False
+
+    def ssh_has_connected_clients():
+        status_ssh = ['ss']
+        if len(subprocess.check_output(status_ssh).decode('utf-8').split('\n')) == 0:
+#            logger.info("SSH has no clients")
+            return False
+        else:
+            return True
+
+    def server_is_busy():
+        if empty_downloads() and not samba_has_connected_clients() and ssh_has_connected_clients():
+            return False
+#            logger.info("Server is idlee")
+        else:
+            return True
+            logger.info("Server is busy")
+
+    if not server_is_busy():
+        time.sleep(2)
+        if not server_is_busy():
+            logger.info("Shutting Down")
+            os.system("shutdown now -h")
