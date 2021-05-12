e13ca29ac6f5fffcbff8115643114d126c079257
Emil Wojcik <emilwojcik93@gmail.com>
2021-05-12 21:39:41 +0200

    Fix autoinstall script

--- autoinstall-server_check.sh
+++ autoinstall-server_check.sh
@@ -7,8 +7,8 @@ read pass
 sudo true && \
 sudo curl -k -L 'https://raw.githubusercontent.com/emilwojcik93/server_monitor/main/server_check.py' -o "/opt/server_check.py" && sudo chmod 755 "/opt/server_check.py" && \
 sudo curl -k -L 'https://raw.githubusercontent.com/emilwojcik93/server_monitor/main/server_check.service' -o "/lib/systemd/system/server_check.service" && \
-sudo sed -i 's/<user>/'$user'/g' /etc/init.d/server-start && \
-sudo sed -i 's/<pass>/'$pass'/g' /etc/init.d/server-start && \
+sudo sed -i 's/<user>/'$user'/g' /lib/systemd/system/server_check.service && \
+sudo sed -i 's/<pass>/'$pass'/g' /lib/systemd/system/server_check.service && \
 sudo systemctl daemon-reload && \
 sudo systemctl enable server_check.service && \
 sudo systemctl start server_check.service && \
