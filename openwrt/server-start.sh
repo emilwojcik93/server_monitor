d3a3e6253cb99f10774a1c4fbd7ffe63d5bcbde7
Emil Wojcik <emilwojcik93@gmail.com>
2021-05-12 21:29:00 +0200

    Modified script for OpenWRT

--- openwrt/server-start.sh
+++ openwrt/server-start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+iface=$1
+mac=$2
 while true; do
   while inotifywait -e modify /var/log/nginx/access.log; do
     if tail -n1 /var/log/nginx/access.log | grep 502; then
-      /usr/bin/etherwake -D -i 'wlan1.sta1' "50:57:A8:E1:44:6B" 2>&1
+      echo "/usr/bin/etherwake -D -i '$iface' "$mac" 2>&1"
       sleep 5
     fi
   done
