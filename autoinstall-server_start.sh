a3807a634e3fd231cc0e77b919345bc31c4711ef
Emil Wojcik <emilwojcik93@gmail.com>
2021-05-13 23:45:05 +0200

    Modify of autoinstall script for server_start.sh, add target arg

--- autoinstall-server_start.sh
+++ autoinstall-server_start.sh
@@ -2,11 +2,14 @@ echo "Enter iface of OpenWRT: "
 read iface
 echo "Enter MAC ADDR of target server: "  
 read mac
+echo "Enter IP ADDR of target server: "  
+read target
 
 curl -k -L 'https://raw.githubusercontent.com/emilwojcik93/server_monitor/main/openwrt/server-start.sh' -o "/bin/server-start.sh" && chmod 755 "/bin/server-start.sh" && \
 curl -k -L 'https://raw.githubusercontent.com/emilwojcik93/server_monitor/main/openwrt/server-start' -o "/etc/init.d/server-start" && chmod 755 "/etc/init.d/server-start" && \
 sed -i 's/<iface>/'$iface'/g' /etc/init.d/server-start && \
 sed -i 's/<mac>/'$mac'/g' /etc/init.d/server-start && \
+sed -i 's/<target>/'$target'/g' /etc/init.d/server-start && \
 /etc/init.d/server-start start && \
 /etc/init.d/server-start enable && \
 ps | grep server-start | grep -v grep
