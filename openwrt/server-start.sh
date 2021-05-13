17309f5f6b5ed6372f14280ccb7508b015c99c7e
Emil Wojcik <emilwojcik93@gmail.com>
2021-05-14 01:01:33 +0200

    Change logic of start-script.sh, update autoinstall script and service for start-scritp.sh

--- openwrt/server-start.sh
+++ openwrt/server-start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 iface=$1
-mac=$2
+skip_ip_addr=$2
 target=$3
+mac=$4
 
-tcpdump -i $iface -U -s0 dst $target | while read -r line; do
+tcpdump -i $iface -U -s0 not src host $skip_ip_addr and not icmp and dst host $target | while read -r line; do
   if ! ping -q -W 1 -c 1 $target >/dev/null; then
     /usr/bin/etherwake -D -i "$iface" "$mac" 2>&1
     sleep 10
   fi
 done
+
